bbb27d3c0bf2ebd6af6ea3da45077c914d35930b
heru-nan <hrwgallardo@gmail.com>
2020-12-06 14:54:04 -0300

    Mailing express type

--- src/controllers/Mailing.js
+++ src/controllers/Mailing.js
@@ -1,36 +1,40 @@
+import nodemailer from 'nodemailer';
+const { google } = require('googleapis');
+const { OAuth2 } = google.auth;
+const OAUTH_PLAYGROUND = 'https://developers.google.com/oauthplayground';
+const {
+  MAILING_SERVICE_CLIENT_ID,
+  MAILING_SERVICE_CLIENT_SECRET,
+  MAILING_SERVICE_REFRESH_TOKEN,
+  SENDER_EMAIL_ADDRESS,
+} = process.env;
 
+const Mailing = {};
 
-Mailing.sendEmail = async (data, res) => {
-  await oauth2Client.setCredentials({
+const oauth2Client = new OAuth2(
+  MAILING_SERVICE_CLIENT_ID,
+  MAILING_SERVICE_CLIENT_SECRET,
+  OAUTH_PLAYGROUND
+);
+
+Mailing.sendEmail = data => {
+  oauth2Client.setCredentials({
     refresh_token: MAILING_SERVICE_REFRESH_TOKEN,
   });
+  const accessToken = oauth2Client.getAccessToken();
 
-
-  const accessToken = await oauth2Client.getAccessToken();
   const smtpTransport = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        type: 'OAuth2',
-        user: SENDER_EMAIL_ADDRESS,
-        clientId: MAILING_SERVICE_CLIENT_ID,
-        clientSecret: MAILING_SERVICE_CLIENT_SECRET,
-        refreshToken: MAILING_SERVICE_REFRESH_TOKEN,
-        accessToken,
-      },
-      tls: {
-        rejectUnauthorized: false
-      }
-    });
-    const content = `<h2>${data.name}</h2><h3>${data.email}</h3><p>${data.message}</p>`;
-    
-  smtpTransport.verify(function(error, success) {
-      if (error) {
-        console.log("error transportador");
-        console.log(error);
-      } else {
-        console.log("Server is ready to take our messages");
-      }
-    });
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: SENDER_EMAIL_ADDRESS,
+      clientId: MAILING_SERVICE_CLIENT_ID,
+      clientSecret: MAILING_SERVICE_CLIENT_SECRET,
+      refreshToken: MAILING_SERVICE_REFRESH_TOKEN,
+      accessToken,
+    },
+  });
+  const content = `<h2>${data.name}</h2><h3>${data.email}</h3><p>${data.message}</p>`;
 
   const mailOptions = {
       from: SENDER_EMAIL_ADDRESS,
@@ -38,20 +42,13 @@ Mailing.sendEmail = async (data, res) => {
       subject: "Contact Me",
       html: content,
     };
-
-  await smtpTransport.sendMail(mailOptions, (err, info) => {
+  smtpTransport.sendMail(mailOptions, (err, info) => {
       if (err) {
-        console.log("Error sendEmail: ");
-        console.log(err);
-        res.status(500).json({
-          res: err,
-          error: 1
-      })
+        console.log("error smtpTransporter");
+        return err;
       };
-      console.log(info);
-
+      console.log("ALl, Ok");
       return info;
-
     });
 };
 
